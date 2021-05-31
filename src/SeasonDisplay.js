3252f6ad75ae99a3d7eee31ce96d299a993f42f3
umairahmed88 <umairahmed_88@ymail.com>
2021-06-01 03:01:00 +0500

    added life cycle method

--- src/SeasonDisplay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SeasonDisplay = () => {
-    return (
-        <div>Seasons Display</div>
-    );
-}
-
-export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay/SeasonDisplay";
+import Spinner from "./spinner";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errMessage: '' };
 
-        this.state = { lat: null, errMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errMessage: err.message });
-            }    
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errMessage: err.message })
         );
     }
+
+    renderContent() {
+        if (this.state.errMessage && !this.state.lat) {
+            return <div>Error: {this.state.errMessage}</div>;
+        }
+
+        if (!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <Spinner message='Please accept location request' />;
+    };
+
     render () {
-            if (this.state.errMessage && !this.state.lat) {
-                return <div>Error: {this.state.errMessage}</div>;
-            }
-
-            if (!this.state.errMessage && this.state.lat) {
-                return <div>Latitude: {this.state.lat}</div>;
-            }
-
-            return <div>Loading!</div>;
+        return (
+            <div className='border aqua'>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
